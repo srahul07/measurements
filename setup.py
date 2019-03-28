d7c8558f25fe988301e1279728724b995f9cc5f8
Rahul Shelke <srahul07@gmail.com>
2019-03-28 18:18:42 +0530

    Updating version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="lengths_converter",
-    version=0.2,
+    version=0.3,
     author="Rahul Shelke",
     author_email="srahul07@gmail.com",
     url="https://bitbucket.org/srahul07/length_converter/src/basic/",
